6c1f5c95c5549882b8f8d2cc3da5bb1db043024e
Vinod <vinod8840@gmail.com>
2022-09-27 22:00:45 +0000

    first commit

---
+++ src/main/java/com/allstate/speedyclaims/SpeedyClaimsApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.speedyclaims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyClaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyClaimsApplication.class, args);
+	}
+
+}
